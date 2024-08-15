a3aa2e16aa02a8b60c4dd42367011bf105d28f7a
PPallavalli <ppallavalli@creare.com>
2024-08-15 15:25:21 -0400

    Comments revamped

--- src/app/utilities/file.service.ts
+++ src/app/utilities/file.service.ts
@@ -83,6 +83,7 @@ export class FileService {
      * Writes specified content to a file at the specified path.
      * @param path - The relative path where the file should be created or written.
      * @param data - The content to be written to the file.
+     * @param rootDir - (Optional) The root directory where the file should be created. Default root directory is used if not specified
      * @returns A promise containing the content URI of the created or specified file.
      */
     async writeFile(path:string, data:string,rootDir:string|undefined=this.rootUri) {
@@ -96,15 +97,21 @@ export class FileService {
         return result
     };
     
-    
+    /**
+     * Checks if the given input string is a content URI.
+     * @param input - The string to check.
+     * @returns A promise that resolves to true if the input is a content URI, otherwise false.
+     */
     async isContentUri(input:string|undefined){
         return input?.startsWith('content://com.android')
     }
 
-    /**
-     * Reads the content from a specified file path or content URI. Must specify either contentURI or file path for it to work successfully and avoid errors
+     /**
+     * Reads the content from a specified file path or content URI.
+     * Must specify either contentURI or file path for it to work successfully and avoid errors.
      * @param path - (Optional) The relative path to the file.
      * @param contentUri - (Optional) The content URI of the file.
+     * @param rootDir - (Optional) The root directory where the file is located. Default root directory (Documents directory in most cases) is used if not specified.
      * @returns A promise containing the file details such as content URI, MIME type, name, size, and content.
      */
     async readFile(input:string|undefined,rootDir:string|undefined=this.rootUri) {
@@ -122,6 +129,7 @@ export class FileService {
     /**
      * Creates a directory at the specified path.
      * @param path - The relative path where the directory should be created.
+     * @param rootDir - (Optional) The root directory where the file is located. Default root directory (Documents directory in most cases) is used if not specified.
      * @returns A promise containing the content URI of the created directory.
      */
     async createDirectory(path:string,rootDir:string|undefined=this.rootUri) {
@@ -136,9 +144,11 @@ export class FileService {
         return result
     }
     /**
-     * Copies a directory from a source path to a destination path. Creates destinationPath if it does not exist but source path must exist to avoid errors
+     * Copies a directory from a source path to a destination path.
+     * Creates destinationPath if it does not exist but source path must exist to avoid errors.
      * @param sourcePath - The source directory path.
      * @param destinationPath - The destination directory path.
+     * @param rootDir - (Optional) The root directory where the source and destination folders are located. Default root directory (Documents directory in most cases) is used if not specified.
      * @returns A promise containing the status of the copy operation.
      */
     async copyDirectory(sourcePath:string,destinationPath:string,rootDir:string|undefined=this.rootUri) {
@@ -167,6 +177,7 @@ export class FileService {
     /**
      * Deletes a directory or file at the specified path.
      * @param path - The relative path to the directory or file to be deleted.
+     * @param rootDir - (Optional) The root directory where the file is located. Default root directory (Documents directory in most cases) is used if not specified.
      * @returns A promise containing the status of the delete operation.
      */
     async deleteDirectory(path:string,rootDir:string|undefined=this.rootUri) {
@@ -181,11 +192,12 @@ export class FileService {
         return result
     }
     /**
-     * Lists the files in a specified directory by either path or content URI. Must specify either the path or the contentUri to avoid errors
-     * @param path - (Optional) The relative path to the directory.
-     * @param contentUri - (Optional) The content URI of the directory.
-     * @returns A promise containing an array of files in the specified directory.
-     */
+ * Lists the files in a specified directory by either path or content URI.
+ * Must specify either the path or the contentUri to avoid errors.
+ * @param input - The relative path to the directory or content URI.
+ * @param rootDir - (Optional) The root directory where the file is located. Default root directory (Documents directory in most cases) is used if not specified.
+ * @returns A promise containing an array of files in the specified directory.
+ */
     async listDirectory(input:string|undefined,rootDir:string|undefined=this.rootUri) {
         let result = null
         try{
