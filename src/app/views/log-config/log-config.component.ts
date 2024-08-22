94b309159247ad80c1a3f4f0872c94c9b47b430a
PPallavalli <ppallavalli@creare.com>
2024-08-22 14:34:37 -0400

    Changes made to display and export application logs

--- src/app/views/log-config/log-config.component.ts
+++ src/app/views/log-config/log-config.component.ts
@@ -4,6 +4,10 @@ import { Tasks } from '../../utilities/tasks.service';
 import { SqLite } from '../../utilities/sqLite.service';
 import { StateModel } from '../../models/state/state.service';
 import { StateInterface } from '../../models/state/state.interface';
+import { FileService } from '../../utilities/file.service';
+import { DialogDataInterface } from '../../interfaces/dialog-data.interface';
+import { DialogType } from '../../utilities/constants';
+import { Notifications } from '../../utilities/notifications.service';
 
 @Component({
   selector: 'log-config-view',
@@ -14,35 +18,66 @@ export class LogConfigComponent {
 
   state: StateInterface;
   showLogs: boolean;
-  logs?: String[];
+  logs?: string[] = [];
 
   constructor(
     public translate: TranslateService,
     public stateModel: StateModel,
     private sqLite: SqLite,
-    private tasks: Tasks
+    private tasks: Tasks,
+    private fileService:FileService,
+    private notifications: Notifications
   ) { 
     this.state = this.stateModel.getState();
     this.showLogs = this.state.isPaneOpen.appLog;
   }
 
   async ngOnInit() {
-    this.logs = await this.sqLite.getAllLogs();
     console.log('LOGS', this.logs);
   }
 
   logsCount = this.sqLite.count['logs'];
 
-  displayLogs() {
-    console.log("displayLogs pressed");
+  async displayLogs() {
+    this.showLogs = !this.showLogs;
+    this.logs = await this.sqLite.getAllLogs();
+    console.log('LOGS ARE----' + this.logs);
   }
 
-  logExportUpload() {
-    console.log("logExport pressed");
-  }
+  // async logExportUpload() {
+      
+  // } 
 
-  logExportSave() {
-    console.log("logExport pressed");
+  async logExportSave() {
+      this.logs = await this.sqLite.getAllLogs();
+      if (!this.logs || this.logs.length==0){
+        console.log("Nothing to Export");
+        return;
+      }
+      let msg: DialogDataInterface = {
+        title: "Confirm Export",
+        content: "Are you sure you want to export the logs to .tabsintlogs?",
+        type: DialogType.Confirm
+      };
+      this.notifications.confirm(msg).subscribe(async (result: string) => {
+        if (result === "OK") {
+          try {
+            const currentTimeStamp = new Date().toISOString();
+            const formattedLogs = this.logs!.map((log, index) => ({
+              msgID: index + 1,
+              date: currentTimeStamp,
+              data: log,
+            }));
+            const logData = JSON.stringify({ logs: formattedLogs }, null, 2);
+            const filename = `.tabsintlogs/${currentTimeStamp}.json`;
+            await this.fileService.writeFile(filename,logData)
+        } catch (error){
+          console.error('Error exporting logs:', error);
+        }
+        } else {
+          console.log('Export canceled.');
+        }
+      });
   }
 
 }
