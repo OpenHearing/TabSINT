adf2ee1138d129c9514da4c1d12f4f09d2e71ee3
Veronique Archambault-Leger <val@creare.com>
2024-09-09 14:58:23 -0400

    few things

--- src/app/guards/type.guard.ts
+++ src/app/guards/type.guard.ts
@@ -1,14 +1,15 @@
 import { PageDefinition, ProtocolReferenceInterface } from "../interfaces/page-definition.interface";
 import { ProtocolSchemaInterface } from "../interfaces/protocol-schema.interface";
+import { PageTypes } from "../types/custom-types";
 
-export function isProtocolSchemaInterface(page: PageT): page is ProtocolSchemaInterface {
+export function isProtocolSchemaInterface(page: PageTypes): page is ProtocolSchemaInterface {
     return (page as ProtocolSchemaInterface).pages !== undefined;
 }
 
-export function isPageDefinition(page: ProtocolSchemaInterface | PageDefinition | ProtocolReferenceInterface): page is PageDefinition {
+export function isPageDefinition(page: PageTypes): page is PageDefinition {
     return (page as PageDefinition).id !== undefined;
 }
 
-export function isProtocolReferenceInterface(page: ProtocolSchemaInterface | PageDefinition | ProtocolReferenceInterface): page is ProtocolReferenceInterface {
+export function isProtocolReferenceInterface(page: PageTypes): page is ProtocolReferenceInterface {
     return (page as ProtocolReferenceInterface).reference !== undefined;
 }
