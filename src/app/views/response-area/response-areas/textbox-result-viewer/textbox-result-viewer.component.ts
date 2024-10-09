6c369242504fcdcdf24dbc0162689e32c5f088a3
Veronique Archambault-Leger <val@creare.com>
2024-10-09 14:36:12 -0400

    textbox-results-viewer correctly displays response

--- src/app/views/response-area/response-areas/textbox-result-viewer/textbox-result-viewer.component.ts
+++ src/app/views/response-area/response-areas/textbox-result-viewer/textbox-result-viewer.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 import { ResultsInterface } from '../../../../models/results/results.interface';
-import { ResultsModel } from '../../../../models/results/results-model.service';
-import { PageModel } from '../../../../models/page/page.service';
 import { PageInterface } from '../../../../models/page/page.interface';
 import { TextBoxResultViewerInterface } from './textbox-result-viewer.interface';
+
+import { ResultsModel } from '../../../../models/results/results-model.service';
+import { PageModel } from '../../../../models/page/page.service';
 
 @Component({
   selector: 'textbox-result-viewer-view',
   templateUrl: './textbox-result-viewer.component.html'
 })
-export class TextboxResultViewerComponent {
+export class TextboxResultViewerComponent implements OnInit, OnDestroy {
   currentPage: PageInterface;
   results: ResultsInterface;
   response: string = '';
+  pageSubscription: Subscription | undefined;
 
   constructor (
     private readonly resultsModel: ResultsModel, 
@@ -21,10 +25,22 @@ export class TextboxResultViewerComponent {
     this.results = this.resultsModel.getResults();
     this.currentPage = this.pageModel.getPage();
     
-    if (this.currentPage.responseArea?.type === "textboxResponseAreaResultViewer") {
-      const textboxResponseAreaResultViewer = this.currentPage.responseArea as TextBoxResultViewerInterface;
-      this.response = this.results.currentExam.responses.find( (response: {pageId: string}) => response.pageId === textboxResponseAreaResultViewer.pageIdToDisplay).response;
-      
-    }
   }
+  
+  ngOnInit() {
+    this.pageSubscription = this.pageModel.currentPageSubject.subscribe( (updatedPage: PageInterface) => {
+      if (updatedPage.responseArea?.type === "textboxResponseAreaResultViewer") {
+        const textboxResponseAreaResultViewer = updatedPage.responseArea as TextBoxResultViewerInterface;
+        let reverseResponses = [...this.results.currentExam.responses].reverse();
+        let findResultOfPageIdToDisplay = ( response: {pageId: string} ) => 
+          response.pageId === textboxResponseAreaResultViewer.pageIdToDisplay        
+        this.response = reverseResponses.find(findResultOfPageIdToDisplay).response;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.pageSubscription?.unsubscribe();
+  }
+
 }
