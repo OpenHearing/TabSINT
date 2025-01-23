edf61d5219b1860cc8a210a9eab0d92c350ebc17
BAyotte <BAyotte@creare.com>
2025-01-23 11:15:07 -0500

    Adding uuid to welcome page

--- src/app/views/build-details/build-details.component.ts
+++ src/app/views/build-details/build-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { VersionModel } from '../../models/version/version.service';
 import { VersionInterface } from '../../models/version/version.interface';
 import { Logger } from '../../utilities/logger.service';
+import { DevicesModel } from '../../models/devices/devices-model.service';
+import { DevicesInterface } from '../../models/devices/devices.interface';
 
 @Component({
   selector: 'build-details',
@@ -10,8 +12,15 @@ import { Logger } from '../../utilities/logger.service';
 })
 export class BuildDetailsComponent implements OnInit {
   version: VersionInterface | undefined;
+  devices: DevicesInterface | undefined;
 
-  constructor(private readonly versionModel: VersionModel, private readonly logger: Logger) {  }
+  constructor(
+    private readonly versionModel: VersionModel, 
+    private readonly logger: Logger,
+    private readonly devicesModel: DevicesModel
+  ) {
+    this.devices = this.devicesModel.getDevices();
+  }
 
   ngOnInit(): void {
     this.initializeVersion();
