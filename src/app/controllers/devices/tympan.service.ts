01977393b8777c0dbed13e0d534437b87b34db97
BAyotte <BAyotte@creare.com>
2025-08-15 13:19:36 -0400

    implemented byte timeout

--- src/app/controllers/devices/tympan.service.ts
+++ src/app/controllers/devices/tympan.service.ts
@@ -42,7 +42,7 @@ export class TympanService {
         this.tympanSubscription = this.devicesModel.tympanResponseSubject.subscribe((response: TympanResponse) => {
             logger.debug("tympanService device msg: "+JSON.stringify(response));
             if (this.deviceUtil.isResponseInvalidChecksum(response)) {
-                this.retryTympanCommand();
+                this.retryTympanCommand();            
             } else if (this.deviceUtil.doTympanResponseMsgIdsMatch(this.pendingMsgInfo, response)) {
                 this.pendingMsg = false;
                 this.response = JSON.parse(response.msg);
@@ -142,7 +142,7 @@ export class TympanService {
             let maxByteLength = this.deviceUtil.getMaxByteLengthFromDeviceId(tympanId);
             await this.tympanWrap.write(tympanId, msg, maxByteLength);
             await this.waitForResponse();
-            resp = this.response.length === 0 ? [-msgId,"ERROR","timeout"] : this.response;
+            resp = this.handleTimeoutErrors(msgId);
         } catch (e) {
             this.state.examState = ExamState.DeviceError;
             this.state.deviceError = resp;
@@ -242,6 +242,18 @@ export class TympanService {
         this.logger.error('INVALID CHECKSUM in Tympan Service');
         this.logger.error("RETRYING COMMAND " + String(this.currentCommand?.func));
         await this.currentCommand?.func(...this.currentCommand.params);
+    }
+
+    private handleTimeoutErrors(msgId: string) {
+        let resp;
+        if (this.response.length === 0) {
+            resp = [-msgId,"ERROR","timeout"];
+        } else if (JSON.stringify(this.response).includes("byte timeout")) {
+            resp = [-msgId,"ERROR","byte timeout"];
+        } else {
+            resp = this.response;
+        }
+        return resp
     }
 }
 
