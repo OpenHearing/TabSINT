967b5dad0243b50af57f394db24581b2145b0bae
Sam Cranford <scranford@creare.com>
2026-03-26 11:34:11 -0400

    Fix unit tests for pipelines to pass

--- src/app/views/response-area/response-areas/qr-code/qr-code.component.spec.ts
+++ src/app/views/response-area/response-areas/qr-code/qr-code.component.spec.ts
@@ -6,6 +6,7 @@ import { PageModel } from '../../../../models/page/page.service';
 import { QrCodeResponseAreaComponent } from './qr-code.component';
 import { QrService } from '../../../../services/qr.service';
 import { Notifications } from '../../../../services/notifications.service';
+import { TranslateFakeLoader, TranslateLoader, TranslateModule, TranslateService, TranslateStore } from '@ngx-translate/core';
 
 describe('QrCodeResponseAreaComponent', () => {
   let component: QrCodeResponseAreaComponent;
@@ -14,8 +15,15 @@ describe('QrCodeResponseAreaComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [QrCodeResponseAreaComponent],
-      imports: [],
-      providers: [StateModel, ResultsModel, PageModel, QrService, Notifications],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader,
+          },
+        }),
+      ],
+      providers: [TranslateService, TranslateStore, StateModel, ResultsModel, PageModel, QrService, Notifications],
     }).compileComponents();
 
     fixture = TestBed.createComponent(QrCodeResponseAreaComponent);
