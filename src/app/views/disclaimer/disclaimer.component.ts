ce2f30ac078099d797391a5f36ae5bc45e5d7411
BAyotte <BAyotte@creare.com>
2026-03-27 12:45:51 -0400

    auto updating the copyright year in app disclaimer

--- src/app/views/disclaimer/disclaimer.component.ts
+++ src/app/views/disclaimer/disclaimer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
@@ -8,22 +8,24 @@ import { DiskInterface } from '../../models/disk/disk.interface';
 import { DiskModel } from '../../models/disk/disk.service';
 
 @Component({
-  selector: 'disclaimer-view',
+  selector: 'app-disclaimer-view',
   standalone: true,
   templateUrl: './disclaimer.component.html',
   styleUrl: './disclaimer.component.css',
   imports: [FormsModule, TranslateModule],
 })
-export class DisclaimerComponent {
+export class DisclaimerComponent implements OnInit, OnDestroy {
+  private readonly dialog = inject(MatDialog);
+  private readonly diskModel = inject(DiskModel);
+
   disk: DiskInterface;
   diskSubscription: Subscription | undefined;
   pin: number | undefined;
+  copyrightYearRange: string;
 
-  constructor(
-    private readonly dialog: MatDialog,
-    private readonly diskModel: DiskModel
-  ) {
+  constructor() {
     this.disk = this.diskModel.getDisk();
+    this.copyrightYearRange = '2015-' + new Date().getFullYear().toString();
   }
 
   ngOnInit(): void {
