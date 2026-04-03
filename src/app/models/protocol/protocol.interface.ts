96887f199433fbf0c6aa1bdd588d13c90bd989f8
VAL (using Claude) <val@creare.com>
2026-04-03 17:38:28 +0000

    CHORE: cleanup unused schema fields
    (cherry picked from commit 72a337cd9d369b84bc9512ec719b177f1c619d5b)

--- src/app/models/protocol/protocol.interface.ts
+++ src/app/models/protocol/protocol.interface.ts
@@ -28,7 +28,6 @@ export interface ProtocolModelInterface {
   activeProtocolFollowOnsDictionary?: FollowOnsDictionary;
 }
 export interface ProtocolInterface extends ProtocolSchemaInterface, ProtocolMetaInterface {
-  key?: string;
   commonRepo?: MediaReposInterface;
   cCommon?: CalibrationFileInterface;
   protocolTabsintOutdated?: boolean;
@@ -42,9 +41,7 @@ export interface ProtocolInterface extends ProtocolSchemaInterface, ProtocolMeta
   _missingWavCalList?: string[];
   _missingCommonWavCalList?: string[];
   _missingControllerList?: string[];
-  _missingHtmlList?: string[];
   _missingCommonMediaRepo?: boolean;
-  _hasSubjectIdResponseArea?: boolean;
   _customHtmlList?: {
     name: string;
     path: string;
