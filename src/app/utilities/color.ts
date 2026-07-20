0427b8c512f6e484a5480e23efb1eaff20819d81
Sam Cranford <scranford@creare.com>
2026-07-20 09:18:34 -0400

    Add color constants

---
+++ src/app/utilities/color.ts
@@ -0,0 +1,9 @@
+/**
+ * Define the color palette for mappings to CSS.
+ */
+export enum AppColor {
+  Success = 'var(--color-success)',
+  Warning = 'var(--color-warning)',
+  Danger = 'var(--color-danger)',
+  Unknown = 'var(--color-unknown)',
+}
