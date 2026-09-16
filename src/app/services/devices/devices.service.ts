f30d42cd5d33c51ba1934cf1bcd9cfe997f1d11b
Sam Cranford <scranford@creare.com>
2026-09-16 14:43:42 -0400

    Improve device or default check

--- src/app/services/devices/devices.service.ts
+++ src/app/services/devices/devices.service.ts
@@ -232,15 +232,15 @@ export class DevicesService {
   async getDeviceOrDefault(tabsintId: string | undefined, defaultTypes: DeviceType[]): Promise<IDevice[]> {
     const devices = await firstValueFrom(this.devices);
     const availableDevices: IDevice[] = [];
-    const device = tabsintId === undefined ? undefined : devices.find(device => device.tabsintId == tabsintId);
-    if (device) {
-      availableDevices.push(structuredClone(device));
-    } else {
-      const foundDevices = devices.filter(device => defaultTypes.includes(device.type) && device.state === DeviceState.Connected);
-      foundDevices.forEach(dev => {
-        availableDevices.push(structuredClone(dev));
-      });
-    }
+    const useDefaults = tabsintId === undefined;
+    const foundDevices = devices.filter(
+      device =>
+        device.state === DeviceState.Connected &&
+        ((useDefaults && defaultTypes.includes(device.type)) || (!useDefaults && tabsintId === device.tabsintId))
+    );
+    foundDevices.forEach(dev => {
+      availableDevices.push(structuredClone(dev));
+    });
     return availableDevices;
   }
 
